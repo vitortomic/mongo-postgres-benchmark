cf4bd1790c60716e6e09561f87f40efb95bceaa5
Vitor Tomic <vitor.tomic@vicert.com>
2024-02-07 00:21:16 +0100

    add pg to benchmark

---
+++ postgresPersonService.js
@@ -0,0 +1,176 @@
+const { Pool } = require('pg');
+const { performance } = require('perf_hooks');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'admin',
+  port: 5433,
+});
+
+const createTablesPg = async () => {
+  const client = await pool.connect();
+
+  try {
+    await client.query('BEGIN');
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS person (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        birth_date DATE
+      )
+    `);
+
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS contact (
+        id SERIAL PRIMARY KEY,
+        person_id INTEGER REFERENCES person(id),
+        address VARCHAR(255),
+        phone_number VARCHAR(20)
+      )
+    `);
+
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.error('Error creating tables:', error.message);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+const createPersonPg = async (personDto) => {
+
+  const client = await pool.connect();
+
+  try {
+    await client.query('BEGIN');
+
+    const personResult = await client.query(
+      'INSERT INTO person(first_name, last_name, birth_date) VALUES($1, $2, $3) RETURNING *',
+      [personDto.firstName, personDto.lastName, new Date('1990-01-01')]
+    );
+
+    const contactResult = await client.query(
+      'INSERT INTO contact(person_id, address, phone_number) VALUES($1, $2, $3) RETURNING *',
+      [personResult.rows[0].id, personDto.address, personDto.phoneNumber]
+    );
+
+    await client.query('COMMIT');
+
+    const savedPerson = { ...personResult.rows[0], contact: contactResult.rows[0] };
+    console.log('Person saved:', savedPerson);
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.error('Error creating person:', error.message);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+const createPersonsPg = async (persons) => {
+  const startTime = performance.now();
+
+  const client = await pool.connect();
+
+  try {
+    await client.query('BEGIN');
+
+    const createdPersonsResult = await Promise.all(
+      persons.map(async (personDto) => {
+        const personResult = await client.query(
+          'INSERT INTO person(first_name, last_name, birth_date) VALUES($1, $2, $3) RETURNING *',
+          [personDto.firstName, personDto.lastName, new Date('1990-01-01')]
+        );
+
+        const contactResult = await client.query(
+          'INSERT INTO contact(person_id, address, phone_number) VALUES($1, $2, $3) RETURNING *',
+          [personResult.rows[0].id, personDto.address, personDto.phoneNumber]
+        );
+
+        return { ...personResult.rows[0], contact: contactResult.rows[0] };
+      })
+    );
+
+    await client.query('COMMIT');
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log(`Saved ${createdPersonsResult.length} persons`);
+    return {
+      createdPersons: createdPersonsResult,
+      executionTime: executionTime,
+    };
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.error('Error creating persons:', error.message);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+const deleteAllPersonsPg = async () => {
+  const startTime = performance.now();
+  const client = await pool.connect();
+  try {
+    const resultContact = await client.query('DELETE FROM contact');
+    const resultPerson = await client.query('DELETE FROM person');
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log(`Deleted ${resultContact.rowCount} persons`);
+    console.log(`Deleted ${resultPerson.rowCount} contacts`);
+    return {
+      deletedCount: resultPerson.rowCount,
+      executionTime: executionTime,
+    };
+  } catch (error) {
+    console.error('Error deleting persons:', error.message);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+const calculateCollectiveAgePg = async () => {
+  const startTime = performance.now();
+  const client = await pool.connect();
+  try {
+    const result = await client.query(
+      'SELECT EXTRACT(EPOCH FROM AGE(NOW(), birth_date)) AS collective_age FROM person'
+    );
+
+    const collectiveAgeInSeconds = result.rows[0]?.collective_age || 0;
+    const collectiveAgeInYears = collectiveAgeInSeconds / (365.25 * 24 * 60 * 60);
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log('Collective age of all persons:', collectiveAgeInYears.toFixed(2), 'years');
+    return {
+      collectiveAgeInYears,
+      executionTime: executionTime,
+    };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
+
+module.exports = {
+  createTablesPg,
+  createPersonPg,
+  createPersonsPg,
+  deleteAllPersonsPg,
+  calculateCollectiveAgePg,
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { createPerson, createPersons, deleteAllPersons, calculateCollectiveAge } = require('./mongoPersonService.js')
+const { createTablesPg, createPersonPg, createPersonsPg, deleteAllPersonsPg, calculateCollectiveAgePg } = require('./postgresPersonService.js')
 const Chance = require('chance')
 const { Pool } = require('pg')
 const Knex = require('knex')
@@ -8,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const chance = new Chance()
 
-const numberOfRecords = 100000
+const numberOfRecords = 100
 
 const persons = Array.from({ length: numberOfRecords }, () => ({
   address: chance.address(),
@@ -35,6 +36,18 @@ app.delete('/persons', async (req, res) => {
   }
 });
 
+app.delete('/personsPg', async (req, res) => {
+  try {
+    deletedCount = await deleteAllPersonsPg();
+    res.json({
+      "deletedCount": deletedCount.deletedCount,
+      executionTime: deletedCount.executionTime
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.get('/persons/getCollectiveAge', async (req, res) => {
   try {
     collectiveAge = await calculateCollectiveAge()
@@ -47,17 +60,38 @@ app.get('/persons/getCollectiveAge', async (req, res) => {
   }
 });
 
+app.get('/persons/getCollectiveAgePg', async (req, res) => {
+  try {
+    collectiveAge = await calculateCollectiveAgePg()
+    res.json({
+      "collectiveAge": collectiveAge,
+      executionTime: collectiveAge.executionTime
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.get('/benchmark', async (req, res) => {
   try {
     createdPersonsMongo = await createPersons(persons)
-    console.log(`Create persons execution time ${createdPersonsMongo.executionTime}ms`)
-    collectiveAge = await calculateCollectiveAge()
-    deleteResponse = await deleteAllPersons()
+    collectiveAgeMongo = await calculateCollectiveAge()
+    deleteResponseMongo = await deleteAllPersons()
+    await createTablesPg()
+    createdPersonsPg = await createPersonsPg(persons)
+    collectiveAgePg = await calculateCollectiveAgePg()
+    deleteResponsePg = await deleteAllPersonsPg()
+    console.log(createdPersonsPg)
+    console.log(collectiveAgePg)
+    console.log(deleteResponsePg)
     response = {
       numberOfRecords,
-      'createPersonsExecutionTime': createdPersonsMongo.executionTime,
-      'getCollectiveAgeExecutionTime': collectiveAge.executionTime,
-      'deleteAllExecutionTime': deleteResponse.executionTime
+      'createPersonsExecutionTimeMongo': createdPersonsMongo.executionTime,
+      'getCollectiveAgeExecutionTimeMongo': collectiveAgeMongo.executionTime,
+      'deleteAllExecutionTimeMongo': deleteResponseMongo.executionTime,
+      'createPersonsExecutionTimePg': createdPersonsPg.executionTime,
+      'getCollectiveAgeExecutionTimePg': collectiveAgePg.executionTime,
+      'deleteAllExecutionTimePg': deleteResponsePg.executionTime
     }
     res.json(response)
   } catch (error) {
