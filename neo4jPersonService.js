e7d17ebec58a5f936bb98a2ed82a4ec514408ad8
Vitor Tomic <vitor.tomic@vicert.com>
2024-02-07 22:30:24 +0100

    add neo4j

---
+++ neo4jPersonService.js
@@ -0,0 +1,142 @@
+const neo4j = require('neo4j-driver');
+const { performance } = require('perf_hooks');
+
+const driver = neo4j.driver(
+  'bolt://localhost:7687', 
+  neo4j.auth.basic('neo4j', '12345678')
+);
+
+const session = driver.session();
+
+
+const createPersonNeo4j = async (personDto) => {
+  
+  const result = await session.writeTransaction(async (tx) => {
+    const createPersonQuery = `
+      CREATE (p:Person {firstName: $firstName, lastName: $lastName, birthDate: $birthDate})
+      RETURN p
+    `;
+    const person = {
+      firstName: personDto.firstName,
+      lastName: personDto.lastName,
+      birthDate: personDto.birthDate.toISOString(),
+    }
+  
+    const createPersonResult = await tx.run(createPersonQuery, person);
+
+    const createContactQuery = `
+    CREATE (c:Contact {address: $address, phoneNumber: $phoneNumber})
+    RETURN c
+    `;
+
+    const contact = {
+      address: personDto.address,
+      phoneNumber: personDto.phoneNumber
+    }
+
+    const createContactResult = await tx.run(createContactQuery, contact);
+
+    await tx.run(
+      `
+      MATCH (p:Person {id: $personId}), (c:Contact {id: $contactId})
+      CREATE (p)-[:HAS_CONTACT]->(c)
+      `,
+      {
+        personId: neo4j.int(personDto.id),
+        contactId: neo4j.int(personDto.contact.id),
+      }
+    );
+  
+    return createPersonResult.records[0].get('p').properties;
+  });
+}
+
+const createPersonsNeo4j = async (persons) => {
+  const startTime = performance.now();
+
+  for (const personDto of persons) {
+    const query = `CREATE (p:Person { firstName: $firstName, lastName: $lastName, birthDate: $birthDate})-[:HAS_CONTACT]->(c:Contact {address: $address, phoneNumber: $phoneNumber})`
+
+    const parameters = {}
+    parameters['firstName'] = personDto.firstName;
+    parameters['lastName'] = personDto.lastName;
+    parameters['birthDate'] = personDto.birthDate.toISOString();
+    parameters['address'] = personDto.address;
+    parameters['phoneNumber'] = personDto.phoneNumber;
+      
+    const result = await session.writeTransaction(async (tx) => {
+      await tx.run(query, parameters);
+    });
+  }
+  
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+
+  console.log(`Saved ${persons.length} persons`);
+  return {
+    createdPersons: persons,
+    executionTime: executionTime,
+  };
+}
+  
+
+const deleteAllNodes = async () => {
+  const startTime = performance.now();
+  const session = driver.session();
+
+  try {
+    await session.writeTransaction(async (tx) => {
+      await tx.run('MATCH (n) DETACH DELETE n');
+    });
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log(`Deleted all nodes`);
+    return {
+      executionTime: executionTime,
+    };
+  } finally {
+    await session.close();
+  }
+};
+
+const calculateCollectiveAgeNeo4j = async () => {
+  const startTime = performance.now();
+  const session = driver.session();
+
+  try {
+    const result = await session.readTransaction(async (tx) => {
+      const collectiveAgeResult = await tx.run(`
+      MATCH (p:Person)
+      WITH toInteger(duration.inSeconds(datetime(p.birthDate), datetime()).seconds) AS ageInSeconds
+      RETURN REDUCE(s = 0, age IN COLLECT(ageInSeconds) | s + age) AS collectiveAgeInSeconds;
+      `);
+
+      const collectiveAgeInSeconds = Number(collectiveAgeResult.records[0].get('collectiveAgeInSeconds'));
+      const collectiveAgeInYears = collectiveAgeInSeconds / (365.25 * 24 * 60 * 60);
+
+      return {
+        collectiveAgeInYears,
+      };
+    });
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+
+    console.log('Collective age of all persons:', result.collectiveAgeInYears.toFixed(2), 'years');
+    return {
+      collectiveAgeInYears: result.collectiveAgeInYears,
+      executionTime: executionTime,
+    };
+  } finally {
+    await session.close();
+  }
+};
+
+module.exports = {
+  createPersonNeo4j,
+  createPersonsNeo4j,
+  deleteAllNodes,
+  calculateCollectiveAgeNeo4j
+}
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express')
 const { createPerson, createPersons, deleteAllPersons, calculateCollectiveAge } = require('./mongoPersonService.js')
 const { createTablesPg, createPersonPg, createPersonsPg, deleteAllPersonsPg, calculateCollectiveAgePg } = require('./postgresPersonService.js')
+const { createPersonNeo4j, createPersonsNeo4j, deleteAllNodes, calculateCollectiveAgeNeo4j } = require('./neo4jPersonService.js')
 const Chance = require('chance')
-const { Pool } = require('pg')
-const Knex = require('knex')
+const { randomUUID } = require('crypto');
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const chance = new Chance()
 
-const numberOfRecords = 100
-
-const persons = Array.from({ length: numberOfRecords }, () => ({
-  address: chance.address(),
-  phoneNumber: chance.phone(),
-  firstName: chance.first(),
-  lastName: chance.last(),
-  birthDate: chance.birthday()
-}));
 
 app.use(express.json())
 app.listen(PORT, () => {
@@ -26,13 +17,13 @@ app.listen(PORT, () => {
 
 app.delete('/persons', async (req, res) => {
   try {
-    deletedCount = await deleteAllPersons();
+    deletedCount = await deleteAllPersons()
     res.json({
       "deletedCount": deletedCount.deletedCount,
       executionTime: deletedCount.executionTime
     });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
 });
 
@@ -44,7 +35,7 @@ app.delete('/personsPg', async (req, res) => {
       executionTime: deletedCount.executionTime
     });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
 });
 
@@ -56,7 +47,7 @@ app.get('/persons/getCollectiveAge', async (req, res) => {
       executionTime: collectiveAge.executionTime
     })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
 });
 
@@ -68,34 +59,70 @@ app.get('/persons/getCollectiveAgePg', async (req, res) => {
       executionTime: collectiveAge.executionTime
     })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
 });
 
-app.get('/benchmark', async (req, res) => {
+app.delete('/deleteAll', async (req, res) => {
   try {
+    deletedCountPg = await deleteAllPersonsPg()
+    deletedCountMongo = await deleteAllPersons()
+    deleteResponseNeo4j = await deleteAllNodes()
+    res.json({
+      "deleteExecutionTimePg": deletedCountPg.executionTime,
+      "deleteExecutionTimeMongo": deletedCountMongo.executionTime,
+      "deleteExecutionTimeNeo4j": deleteResponseNeo4j.executionTime
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+});
+
+app.get('/benchmark/:numberOfRecords', async (req, res) => {
+  try {
+    const numberOfRecords = parseInt(req.params.numberOfRecords)
+
+    if (isNaN(numberOfRecords) || numberOfRecords <= 0) {
+      return res.status(400).json({ error: 'Invalid number!' });
+    }
+
+    const persons = Array.from({ length: numberOfRecords }, () => ({
+      id: randomUUID(),
+      address: chance.address(),
+      phoneNumber: chance.phone(),
+      firstName: chance.first(),
+      lastName: chance.last(),
+      birthDate: chance.birthday()
+    }));
+
     createdPersonsMongo = await createPersons(persons)
     collectiveAgeMongo = await calculateCollectiveAge()
-    deleteResponseMongo = await deleteAllPersons()
+    //deleteResponseMongo = await deleteAllPersons()
+
     await createTablesPg()
     createdPersonsPg = await createPersonsPg(persons)
     collectiveAgePg = await calculateCollectiveAgePg()
-    deleteResponsePg = await deleteAllPersonsPg()
-    console.log(createdPersonsPg)
-    console.log(collectiveAgePg)
-    console.log(deleteResponsePg)
+    //deleteResponsePg = await deleteAllPersonsPg()
+
+    createdPersonsNeo4j = await createPersonsNeo4j(persons)
+    collectiveAgeNeo4j = await calculateCollectiveAgeNeo4j()
+    //deleteResponseNeo4j = await deleteAllNodes()
+
     response = {
       numberOfRecords,
       'createPersonsExecutionTimeMongo': createdPersonsMongo.executionTime,
       'getCollectiveAgeExecutionTimeMongo': collectiveAgeMongo.executionTime,
-      'deleteAllExecutionTimeMongo': deleteResponseMongo.executionTime,
+      //'deleteAllExecutionTimeMongo': deleteResponseMongo.executionTime,
       'createPersonsExecutionTimePg': createdPersonsPg.executionTime,
       'getCollectiveAgeExecutionTimePg': collectiveAgePg.executionTime,
-      'deleteAllExecutionTimePg': deleteResponsePg.executionTime
+      //'deleteAllExecutionTimePg': deleteResponsePg.executionTime,
+      'createPersonsExecutionTimeNeo4j': createdPersonsNeo4j.executionTime,
+      'getCollectiveAgeExecutionTimeNeo4j': collectiveAgeNeo4j.executionTime,
+      //'deleteAllExecutionTimeNeo4j': deleteResponseNeo4j.executionTime
     }
     res.json(response)
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
 });
 
